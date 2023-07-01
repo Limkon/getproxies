9e42b454e05d949901ed8a47f3a2545f2417a4e6
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-01 16:22:40 +0800

    Update process_vpn.py

--- tools/process_vpn.py
+++ tools/process_vpn.py
@@ -1,6 +1,5 @@
 import os
 import base64
-import socket
 import yaml
 import sys
 import json
@@ -24,21 +23,34 @@ def convert_json_to_v2ray(json_data):
 
     return v2ray_servers
 
+def convert_yaml_to_v2ray(yaml_data):
+    v2ray_servers = []
+
+    # 遍历 YAML 数据中的每个服务器
+    for server in yaml_data:
+        # 从服务器对象中提取地址、端口、用户ID和alterId等参数
+        address = server.get("address")
+        port = server.get("port")
+        user_id = server.get("user_id")
+        alter_id = server.get("alter_id")
+
+        # 构建 V2Ray（Vmess）格式的字符串
+        v2ray_server = f"vmess://{base64.b64encode(f'{address}:{port}/{user_id}:{alter_id}'.encode()).decode()}"
+
+        v2ray_servers.append(v2ray_server)
+
+    return v2ray_servers
 
 def get_file_type(file_path):
     mime = magic.Magic(mime=True)
     file_type = mime.from_file(file_path)
     return file_type
 
-
 def process_data_files(data_dir, output_file):
-    # 读取数据文件列表
-    data_files = os.listdir(data_dir)
-
     merged_content = []
 
     # 遍历数据文件，逐个检测内容并处理
-    for file in data_files:
+    for file in os.listdir(data_dir):
         file_path = os.path.join(data_dir, file)
         with open(file_path, "r") as f:
             content = f.read()
@@ -54,7 +66,7 @@ def process_data_files(data_dir, output_file):
                     # os.remove(file_path)  # 删除原始文件
                 except Exception as e:
                     print(f"Error processing JSON file {file}: {str(e)}")
-            elif file_type == 'text/plain':
+            elif file_type == 'text/plain' or file_type == 'application/x-yaml':
                 # 如果是 YAML 文件，尝试将其转换为 V2Ray（Vmess）格式
                 try:
                     yaml_data = yaml.safe_load(content)
@@ -83,7 +95,7 @@ def process_data_files(data_dir, output_file):
                     # os.remove(file_path)  # 删除原始文件
                 except Exception as e:
                     # 内容不是 Base64 编码，继续检测是否符合特定格式
-                    if content.startswith("vmess://") or content.startswith("clash://") or content.startswith("ss://") or content.startswith("vlss://") or content.startswith("trojan://"):
+                    if content.startswith(("vmess://", "clash://", "ss://", "vlss://", "trojan://")):
                         merged_content.append(content)
                         print(f"Processed special format file: {file}")
                         # os.remove(file_path)  # 删除原始文件
@@ -101,6 +113,10 @@ def process_data_files(data_dir, output_file):
             f.write(content + "\n")
 
 # 通过命令行参数传递目录和保存文件名
+if len(sys.argv) < 3:
+    print("Usage: python convert.py <data_directory> <output_file>")
+    sys.exit(1)
+
 data_dir = sys.argv[1]  # 第一个命令行参数为目录
 output_file = sys.argv[2]  # 第二个命令行参数为保存文件名
 
