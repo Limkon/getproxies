7ea5f04e8a8bf744c6820bfab228dc3aeb9ae51f
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-01 17:30:24 +0800

    Update process_vpn.py

--- tools/process_vpn.py
+++ tools/process_vpn.py
@@ -1,76 +1,78 @@
 import os
-import base64
-import yaml
+import re
 import sys
-import json
-import magic
+import time
+import datetime
+import requests
+import concurrent.futures
+import base64
 
-def get_file_type(file_path):
-    mime = magic.Magic(mime=True)
-    file_type = mime.from_file(file_path)
-    return file_type
+from bs4 import BeautifulSoup
 
-def process_data_files(data_dir, output_dir, rest_file):
-    # 创建保存目录（如果不存在）
-    os.makedirs(output_dir, exist_ok=True)
 
-    rest_content = []
+def extract_content(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content = soup.get_text()
+        return content
+    else:
+        raise Exception(f"Failed to fetch content from URL: {url}")
 
-    # 遍历数据文件，逐个检测内容并处理
-    for file in os.listdir(data_dir):
-        file_path = os.path.join(data_dir, file)
-        with open(file_path, "r") as f:
-            content = f.read()
 
-            file_type = get_file_type(file_path)
-            if file_type == 'application/json':
-                # 将文件移到指定目录并将后缀名改为 .json
-                new_file_path = os.path.join(output_dir, file + ".json")
-                os.rename(file_path, new_file_path)
-                print(f"Moved JSON file: {file}")
+def save_content(content, output_dir, url, file_name):
+    file_path = os.path.join(output_dir, file_name)
+    with open(file_path, 'a', encoding='utf-8') as file:
+        file.write(content + '\n')
+    print(f"网站 {url} 内容已保存至文件：{file_path}")
 
-            elif file_type == 'text/plain' or file_type == 'application/x-yaml':
-                # 将文件移到指定目录并将后缀名改为 .yaml
-                new_file_path = os.path.join(output_dir, file + ".yaml")
-                os.rename(file_path, new_file_path)
-                print(f"Moved YAML file: {file}")
 
-            elif file_type == 'application/octet-stream':
-                try:
-                    # 尝试解密 Base64 编码的内容
-                    decoded_content = base64.b64decode(content).decode()
-
-                    # 将解密后的内容追加保存到 rest.txt 文件中
-                    rest_content.append(decoded_content)
-
-                    print(f"Processed Base64 file: {file}")
-                except Exception as e:
-                    # 内容不是 Base64 编码，继续检测是否符合特定格式
-                    if content.startswith(("vmess://", "clash://", "ss://", "vlss://", "trojan://")):
-                        # 将特定格式的内容追加保存到 rest.txt 文件中
-                        rest_content.append(content)
-
-                        print(f"Processed special format file: {file}")
-                    else:
-                        # 内容既不是 Base64 编码也不符合特定格式，打印错误信息
-                        print(f"Error processing file {file}: Content is neither Base64 encoded nor has a special format.")
-
+def process_url(url, output_dir, file_name):
+    try:
+        time.sleep(2)  # 等待页面加载
+        content = extract_content(url)
+        if content:
+            if re.match(r'^(vmess://|clash://|ss://|vlss://|trojan://)', content.strip()):
+                save_content(content, output_dir, url, file_name)
             else:
-                print(f"Warning: Unknown file type for file {file}")
+                try:
+                    decoded_content = base64.b64decode(content).decode('utf-8')
+                    if re.match(r'^(vmess://|clash://|ss://|vlss://|trojan://)', decoded_content.strip()):
+                        save_content(decoded_content, output_dir, url, file_name)
+                except Exception:
+                    pass
+        return f"处理 {url} 完成"
+    except Exception as e:
+        return f"处理 {url} 失败：{str(e)}"
 
-    # 将 rest.txt 文件保存到指定目录
-    with open(rest_file, "a") as rest:
-        for content in rest_content:
-            rest.write(content + "\n")
 
-# 通过命令行参数传递目录和保存目录
-if len(sys.argv) < 4:
-    print("Usage: python convert.py <data_directory> <output_directory> <rest_file>")
-    sys.exit(1)
+def process_urls(urls, output_dir, file_name, num_threads):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = [executor.submit(process_url, url, output_dir, file_name) for url in urls]
 
-data_dir = sys.argv[1]  # 第一个命令行参数为目录
-output_dir = sys.argv[2]  # 第二个命令行参数为保存目录
-rest_file = sys.argv[3]  # 第三个命令行参数为保存的 rest.txt 文件名
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            print(result)
 
-# 处理数据文件
-process_data_files(data_dir, output_dir, rest_file)
+
+def main():
+    if len(sys.argv) != 5:
+        print("请提供要抓取的 URL 列表文件名、保存提取内容的目录、文件名和线程数")
+        print("示例: python extract_urls.py urls.txt data output.txt 10")
+        sys.exit(1)
+
+    urls_file = sys.argv[1]  # 存储要抓取的 URL 列表的文件名
+    output_dir = sys.argv[2]  # 保存提取内容的目录
+    file_name = sys.argv[3]   # 保存内容的文件名
+    num_threads = int(sys.argv[4])  # 线程数
+
+    with open(urls_file, 'r', encoding='utf-8') as file:
+        urls = [line.strip() for line in file]
+
+    process_urls(urls, output_dir, file_name, num_threads)
+
+    print('所有网站内容保存完成！')
+
+
+if __name__ == '__main__':
+    main()
