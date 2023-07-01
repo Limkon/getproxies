05a71fd60b3fc30498f1749591a1305d7574c701
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-01 20:43:54 +0800

    Update readweb.py

--- tools/readweb.py
+++ tools/readweb.py
@@ -3,67 +3,79 @@ import re
 import sys
 import time
 import datetime
+import random
 import requests
 import concurrent.futures
-
+import logging
 from bs4 import BeautifulSoup
+
+# 设置日志记录
+logging.basicConfig(filename='log.txt', level=logging.INFO)
 
 
 def extract_content(url):
-    response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        headers = {
+            'User-Agent': random_user_agent()  # 随机选择一个User-Agent
+        }
+        response = requests.get(url, headers=headers, timeout=5)
+        response.raise_for_status()  # 检查请求是否成功
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # 尝试不同的选择器
         selectors = [
-            '#app',                 # ID 选择器
-            '.content',             # 类选择器
-            'div',                  # 元素选择器
-            '.my-class',            # 类选择器
-            '#my-id',               # ID 选择器
-            '[name="my-name"]',     # 属性选择器
-            '.my-parent .my-child', # 后代选择器
+            '#app',
+            '.content',
+            'div',
+            '.my-class',
+            '#my-id',
+            '[name="my-name"]',
+            '.my-parent .my-child',
         ]
 
         for selector in selectors:
             try:
                 element = soup.select_one(selector)
                 if element:
-                    title = element.select_one('h1.title').get_text().strip()  # 选择标题
-                    content = element.select_one('.content').get_text().strip()  # 选择正文内容
-                    return title, content
+                    content = element.get_text()
+                    return content.strip()
             except Exception as e:
-                print(f"尝试通过选择器 {selector} 获取 {url} 内容失败：{str(e)}")
+                logging.error(f"尝试通过选择器 {selector} 获取 {url} 内容失败：{str(e)}")
 
-        # 如果所有选择器都失败，则执行自定义的处理方法
-        print(f"所有选择器都无法获取 {url} 的内容，将执行自定义代码")
+        logging.info(f"所有选择器都无法获取 {url} 的内容，将执行自定义代码")
 
         # 在此编写自定义的处理方法来选择和提取页面内容
-        # 例如：提取页面的标题和正文内容
-        title = soup.select_one('h1.title').get_text().strip()  # 选择标题
-        content = soup.select_one('.content').get_text().strip()  # 选择正文内容
-        return title, content
-    else:
-        raise Exception(f"Failed to fetch content from URL: {url}")
+        # 例如：提取页面的文本内容
+        content = soup.get_text()
+        return content.strip()
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"请求 {url} 发生异常：{str(e)}")
+    except Exception as e:
+        logging.error(f"处理 {url} 失败：{str(e)}")
+
+    return None
 
 
-def save_content(title, content, output_dir, url):
+def save_content(content, output_dir, url):
     date = datetime.datetime.now().strftime('%Y-%m-%d')
     url_without_protocol = re.sub(r'^(https?://)', '', url)
     url_without_protocol = re.sub(r'[:?<>|\"*\r\n/]', '_', url_without_protocol)
-    url_without_protocol = url_without_protocol[:20]  # 限制文件名长度不超过20个字符
+    url_without_protocol = url_without_protocol[:20]
     file_name = os.path.join(output_dir, url_without_protocol + "_" + date + ".txt")
     with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(f"Title: {title}\n\n{content}")
-    print(f"网站 {url} 内容已保存至文件：{file_name}")
+        file.write(content)
+    logging.info(f"网站 {url} 内容已保存至文件：{file_name}")
 
 
 def process_url(url, output_dir):
     try:
-        time.sleep(2)  # 等待页面加载
-        title, content = extract_content(url)
-        save_content(title, content, output_dir, url)
-        return f"处理 {url} 成功"
+        time.sleep(2)
+        content = extract_content(url)
+        if content:
+            save_content(content, output_dir, url)
+            return f"处理 {url} 成功"
+        else:
+            return f"处理 {url} 失败：无法提取内容"
     except Exception as e:
         return f"处理 {url} 失败：{str(e)}"
 
@@ -75,6 +87,18 @@ def process_urls(urls, output_dir, num_threads):
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
             print(result)
+            logging.info(result)
+
+
+def random_user_agent():
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        # 添加更多的 User-Agent
+    ]
+    return random.choice(user_agents)
 
 
 def main():
@@ -83,9 +107,9 @@ def main():
         print("示例: python extract_urls.py urls.txt data 10")
         sys.exit(1)
 
-    urls_file = sys.argv[1]  # 存储要抓取的 URL 列表的文件名
-    output_dir = sys.argv[2]  # 保存提取内容的目录
-    num_threads = int(sys.argv[3])  # 线程数
+    urls_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    num_threads = int(sys.argv[3])
 
     with open(urls_file, 'r', encoding='utf-8') as file:
         urls = [line.strip() for line in file]
