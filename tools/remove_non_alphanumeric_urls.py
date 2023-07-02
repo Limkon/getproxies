b884b28ee1859699dbeaf8ec70e35e31a991ad72
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-02 22:12:30 +0800

    Update remove_non_alphanumeric_urls.py

--- tools/remove_non_alphanumeric_urls.py
+++ tools/remove_non_alphanumeric_urls.py
@@ -11,13 +11,19 @@ def remove_non_alphanumeric_urls(filename):
 
             # 检查行是否以字母或数字结尾，但保留以斜杠 "/" 结尾的网址
             if re.match(r'^.*[a-zA-Z0-9/]+$', line):
+                # 查找中文字符的位置
+                chinese_index = re.search(r'[\u4e00-\u9fff]', line)
+                if chinese_index:
+                    # 只保留中文字符前的部分
+                    line = line[:chinese_index.start()]
+
                 lines_to_keep.append(line)
 
     # 将结果写回原文件
     with open(filename, 'w') as file:
         file.write('\n'.join(lines_to_keep))
 
-    print("已剔除不以字母或数字结尾（除斜杠“/”外）的网址。")
+    print("已剔除不以字母或数字结尾（除斜杠“/”外）的网址，并删除中文字符及其后面的内容。")
 
 # 获取命令行参数
 if len(sys.argv) != 2:
