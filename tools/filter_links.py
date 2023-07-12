fbdf6b31403eee857beba456072c0d8872a5b99a
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-12 16:56:31 +0800

    Update filter_links.py

--- tools/filter_links.py
+++ tools/filter_links.py
@@ -1,6 +1,4 @@
-import os
 import sys
-import re
 
 # 获取命令行参数中的文件名
 input_file = sys.argv[1]
@@ -11,8 +9,15 @@ valid_prefixes = ['vmess://', 'trojan://', 'clash://', 'ss://', 'vlss://']
 # 读取输入文件并筛选出以特定格式开头的行
 with open(input_file, 'r') as file:
     lines = file.readlines()
+    total_lines = len(lines)
+
     valid_lines = [line for line in lines if any(line.startswith(prefix) for prefix in valid_prefixes)]
+    valid_line_count = len(valid_lines)
 
 # 将筛选后的行写回原文件
 with open(input_file, 'w') as file:
     file.writelines(valid_lines)
+
+# 输出筛选前和筛选后的行数
+print(f"Total lines: {total_lines}")
+print(f"Valid lines: {valid_line_count}")
