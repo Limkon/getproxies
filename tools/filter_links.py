0ef2f6bef375f9fbf493a66d2941ddfe4038c052
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-12 16:44:41 +0800

    Update filter_links.py

--- tools/filter_links.py
+++ tools/filter_links.py
@@ -6,11 +6,11 @@ input_file = sys.argv[1]
 # 定义特定格式的链接前缀列表
 valid_prefixes = ['vmess://', 'trojan://', 'clash://', 'ss://', 'vlss://']
 
-# 读取输入文件并筛选出符合特定格式的链接行
+# 读取输入文件并筛选出以特定格式开头的行
 with open(input_file, 'r') as file:
     lines = file.readlines()
-    valid_lines = [line.strip() for line in lines if line.strip().startswith(tuple(valid_prefixes))]
+    valid_lines = [line for line in lines if any(line.startswith(prefix) for prefix in valid_prefixes)]
 
-# 将筛选后的链接行写回原文件
+# 将筛选后的行写回原文件
 with open(input_file, 'w') as file:
     file.writelines(valid_lines)
