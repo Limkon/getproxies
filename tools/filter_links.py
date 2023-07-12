53e38fb82bffee818ccfe1c7e92f878694be014a
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-12 18:49:17 +0800

    Update filter_links.py

--- tools/filter_links.py
+++ tools/filter_links.py
@@ -1,5 +1,7 @@
-import fileinput
 import sys
+import os
+import tempfile
+import shutil
 
 # 获取命令行参数中的文件名
 input_file = sys.argv[1]
@@ -7,16 +9,21 @@ input_file = sys.argv[1]
 # 定义特定格式的链接前缀列表
 valid_prefixes = ['vmess://', 'trojan://', 'clash://', 'ss://', 'vlss://']
 
-# 备份原始文件
-backup_file = input_file + ".bak"
-with open(input_file, 'r') as file, open(backup_file, 'w') as backup:
-    backup.writelines(file.readlines())
+# 读取输入文件并筛选出以特定格式开头的行
+with open(input_file, 'r') as file:
+    lines = file.readlines()
+    valid_lines = [line for line in lines if any(line.startswith(prefix) for prefix in valid_prefixes)]
 
-# 删除不以特定格式开头的行并保存回原始文件
-with fileinput.FileInput(input_file, inplace=True) as file:
-    for line in file:
-        if any(line.startswith(prefix) for prefix in valid_prefixes):
-            print(line, end='')
+# 创建临时文件来保存筛选后的内容
+temp_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+temp_file.writelines(valid_lines)
+temp_file.close()
+
+# 删除原始文件
+os.remove(input_file)
+
+# 将临时文件重命名为原始文件名
+shutil.move(temp_file.name, input_file)
 
 # 输出处理完成的消息
 print("File processing completed.")
