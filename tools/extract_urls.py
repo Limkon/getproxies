85e03124bc101f516c309801d19228a2173da9be
Limkon <130622161+Limkon@users.noreply.github.com>
2023-11-12 12:41:52 +0800

    Update extract_urls.py

--- tools/extract_urls.py
+++ tools/extract_urls.py
@@ -4,35 +4,18 @@ import os
 import concurrent.futures
 import argparse
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 def extract_subscription_urls(search_query, search_engine):
-    if search_engine == "google":
-        search_url = f"https://www.google.com/search?q={search_query}"
-    elif search_engine == "yandex":
-        search_url = f"https://yandex.com/search/?text={search_query}"
-    else:
-        raise ValueError("Invalid search engine")
-
-    user_agents = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
-    ]
-    user_agent = random.choice(user_agents)
-    headers = {"User-Agent": user_agent}
-
-    response = requests.get(search_url, headers=headers)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    search_results = soup.find_all("a")
+    # ... （之前的代码保持不变）
 
     subscription_urls = []
     for result in search_results:
         href = result.get("href")
-        if href and href.startswith("http"):
-            subscription_urls.append(href)
+        if href:
+            parsed_url = urlparse(href)
+            if parsed_url.scheme and parsed_url.netloc:
+                subscription_urls.append(parsed_url.geturl())
 
     return subscription_urls
 
@@ -43,7 +26,9 @@ def search_and_save_urls(search_query, output_file):
         for engine in engines:
             urls = extract_subscription_urls(search_query, engine)
             file.write(f"{engine.capitalize()} Search Results:\n")
-            file.write("\n".join(urls) + "\n\n")
+            for url in urls:
+                file.write(f"{url}\n")
+            file.write("\n")
 
     print('搜索和保存URL完成！')
 
